51d2b014810d834e71e91cc1e9be2ee31bea65eb
IritaSee <iga.narendra@gmail.com>
2020-11-15 22:35:23 +0800

    add category,forum,post,thread,user ctrlr+migrate

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Category;
+use App\Post;
+use App\Thread;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    public function login(Request $request){
+        
+    }
+}
