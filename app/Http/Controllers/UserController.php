1e19dd72243d8ab60ae41de14b2f68e7ee9c0230
IritaSee <iga.narendra@gmail.com>
2020-11-15 23:39:38 +0800

    create login regist structure

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,6 +12,35 @@ use Illuminate\Support\Facades\Auth;
 class UserController extends Controller
 {
     public function login(Request $request){
-        
+        if(Auth::attempt(['username' => $username , 'password' => $password]))
+        {
+            //return 200 status as json and auth as user
+            return response()->json(Auth::user(), 200);
+        }
+        else
+        {
+            return response()->json(['error' => 401],200);
+        }
     }
+        public function register(Request $request)
+        {
+            //check username exist or not
+            $user = User::where('username',$request->username)->first();
+            if(isset($user->username))
+            {
+                return response()->json(['username_taken' => 401],200);
+            }
+            // then we create the user
+            $user = new User();
+            $user->name = $request->name;
+            $user->username = $request->username;
+            $user->password = bcrypt($request->password);
+            $user->save();
+            //log them in
+            Auth::login($user);
+
+            return response()->json($user,200);
+
+        }
+    
 }
